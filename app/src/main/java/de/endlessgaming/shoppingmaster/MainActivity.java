00bef8062e702f31b68876a2a5ef9ca038cfcae0
WastlSepp <40317478+WastlSepp@users.noreply.github.com>
2020-11-27 20:54:45 +0100

    Added Fragment-Structure

--- app/src/main/java/de/endlessgaming/shoppingmaster/MainActivity.java
+++ app/src/main/java/de/endlessgaming/shoppingmaster/MainActivity.java
@@ -32,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show();
-                //Intent intent=new Intent(LoginActivity.this,MainActivity.class);
+                //Intent intent=new Intent(^^LoginActivity.this,MainActivity.class);
                 //startActivity(intent);
                 Intent intent = new Intent(getApplication(),LoginActivity.class);
                 startActivity(intent);
